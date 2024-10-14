eb036993004ffd99f9e5bb9e11e053260e0e1390
ehirvi <eino.hirvi@gmail.com>
2024-10-14 13:39:57 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  files: ["**/*.ts"],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+  ],
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  ignores: ["build/*"],
+  rules: {
+    "@typescript-eslint/restrict-template-expressions": "off",
+  },
+});
