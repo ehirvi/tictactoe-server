ed3f6612e63203e4732577a041ac815be9c1dbf3
ehirvi <eino.hirvi@gmail.com>
2025-03-16 12:59:54 +0200

    Configure environmental variables into deployment pipeline

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET =
+  process.env.NODE_ENV === "production" ? process.env.JWT_SECRET : "test";
 
 if (!JWT_SECRET) {
-  throw new Error("ENV: SECRET not defined");
+  throw new Error("ENV: JWT_SECRET not defined");
 }
 
 export default { JWT_SECRET };
